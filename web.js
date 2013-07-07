86b2f467718f46be65fe0773120f7d94a8c01de5
rave666 <mikej@gmail.com>
2013-07-07 22:13:31 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require("fs");
+
 app.get('/', function(request, response) {
   var x = fs.readFileSync("./index.html");
   response.send(x);
